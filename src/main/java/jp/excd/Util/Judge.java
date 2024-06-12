61842f1b04416e0c0580291d18d613e43837bad8
yyyyKokubu <yuga.kokubu@excd.jp>
2024-06-12 10:37:31 +0900

    講師レビューの指摘事項を修正

--- src/main/java/jp/excd/Util/Judge.java
+++ src/main/java/jp/excd/Util/Judge.java
@@ -1,4 +1,4 @@
-package jp.excd.judge;
+package jp.excd.Util;
 
 import java.text.DateFormat;
 import java.text.ParseException;
@@ -7,8 +7,13 @@ import java.text.SimpleDateFormat;
 
 public class Judge {
 
-	//日付として妥当な値かどうか判定する。返り値はtrueかfalse
+	/**
+	 * 日付として妥当な値かどうか判定する。返り値はtrueかfalse
+	 * @param value チェック対象値
+	 * @return
+	 */
 	public static boolean isDateValue (String value) {
+		
 		DateFormat format = new SimpleDateFormat("yyyy-MM-dd");
 
 		try {
@@ -17,26 +22,36 @@ public class Judge {
 
 		} catch (ParseException e) {
 			return false;
-
 		}
-
 	}
 
-	//数値として妥当な値かどうか判定する。返り値はtrueかfalse
+	/**
+	 * 数値として妥当な値かどうか判定する。返り値はtrueかfalse
+	 * @param num チェック対象値
+	 * @return
+	 */
 	public static boolean isNumber(String num) {
+		
 		try {
 			Integer.parseInt(num);
 			return true;
+			
 		} catch (NumberFormatException e) {
 			return false; // エラーにならないように、とりあえずダミー
 		}
 	}
 
-	//数値として妥当な値かどうか判定する。返り値はtrueかfalse
+	/**
+	 * 数値として妥当な値かどうか判定する。返り値はtrueかfalse
+	 * @param num チェック対象値
+	 * @return
+	 */
 	public static boolean isDouble(String num) {
+		
 		try {
 			Double.parseDouble(num);
 			return true;
+			
 		} catch (NumberFormatException e) {
 			return false; // エラーにならないように、とりあえずダミー
 		}
--- src/main/java/jp/excd/bean/SongBean.java
+++ src/main/java/jp/excd/bean/SongBean.java
@@ -2,7 +2,6 @@ package jp.excd.bean;
 
 public class SongBean {
 	
-	//プロパティ宣言
 	//曲ID
 	private String song_id;
 	
@@ -55,157 +54,156 @@ public class SongBean {
 	private String other_link_description;
 	
 	
-	//コンストラクタ生成
 	//曲ID
-	public String getsong_id() {
+	public String getSong_id() {
 		return song_id;
 	}
 	
-	public void setsong_id(String song_id) {
+	public void setSong_id(String song_id) {
 		this.song_id = song_id;
 	}
 	
 	//曲名
-	public String gettitle() {
+	public String getTitle() {
 		return title;
 	}
 	
-	public void settitle(String title) {
+	public void setTitle(String title) {
 		this.title = title;
 	}
 	
 	//作曲家ID
-	public String getcomposer_id() {
+	public String getComposer_id() {
 		return composer_id;
 	}
 	
-	public void setcomposer_id(String composer_id) {
+	public void setComposer_id(String composer_id) {
 		this.composer_id = composer_id;
 	}
 	
 	//総感動指数
-	public String getrating_total_formated() {
+	public String getRating_total_formated() {
 		return rating_total_formated;
 	}
 	
-	public void setrating_total_forated(String rating_total_formated) {
+	public void setRating_total_forated(String rating_total_formated) {
 		this.rating_total_formated = rating_total_formated;
 	}
 	
 	//平均感動指数
-	public String getrating_average_formated() {
+	public String getRating_average_formated() {
 		return rating_average_formated;
 	}
 	
-	public void setrating_average_formated(String rating_average_formated) {
+	public void setRating_average_formated(String rating_average_formated) {
 		this.rating_average_formated = rating_average_formated;
 	}
 	
 	//再生回数
-	public String gettotal_listen_count_formated() {
+	public String getTotal_listen_count_formated() {
 		return total_listen_count_formated;
 	}
 	
-	public void settotal_listen_count_formated(String total_listen_count_formated) {
+	public void setTotal_listen_count_formated(String total_listen_count_formated) {
 		this.total_listen_count_formated = total_listen_count_formated;
 	}
 	
 	//公開時間
-	public String getrelease_datetime_formated() {
+	public String getRelease_datetime_formated() {
 		return release_datetime_formated;
 	}
 	
-	public void setrelease_datetime_formated(String release_datetime_formated) {
+	public void setRelease_datetime_formated(String release_datetime_formated) {
 		this.release_datetime_formated = release_datetime_formated;
 	}
 	
 	//最終更新時間
-	public String getlast_update_datetime_formated() {
+	public String getLast_update_datetime_formated() {
 		return last_update_datetime_formated;
 	}
 	
-	public void setlast_update_datetime_formated(String last_update_datetime_formated) {
+	public void setLast_update_datetime_formated(String last_update_datetime_formated) {
 		this.last_update_datetime_formated = last_update_datetime_formated;
 	}
 	
 	//メッセージ
-	public String getmessage() {
+	public String getMessage() {
 		return message;
 	}
 	
-	public void setmessage(String message) {
+	public void setMessage(String message) {
 		this.message = message;
 	}
 	
 	//キー
-	public String getkey_formated() {
+	public String getKey_formated() {
 		return key_formated;
 	}
 	
-	public void setkey_formated(String key_formated) {
+	public void setKey_formated(String key_formated) {
 		this.key_formated = key_formated;
 	}
 	
 	//楽譜表記
-	public String getscore_type_formated() {
+	public String getScore_type_formated() {
 		return score_type_formated;
 	}
 	
-	public void setscore_type_formated(String score_type_formated) {
+	public void setScore_type_formated(String score_type_formated) {
 		this.score_type_formated = score_type_formated;
 	}
 	
 	//BPM
-	public String getbpm() {
+	public String getBpm() {
 		return bpm;
 	}
 	
-	public void setbpm(String bpm) {
+	public void setBpm(String bpm) {
 		this.bpm = bpm;
 	}
 	
 	//イメージファイル名
-	public String getimage_file_name() {
+	public String getImage_file_name() {
 		return image_file_name;
 	}
 	
-	public void setimage_file_name(String image_file_name) {
+	public void setImage_file_name(String image_file_name) {
 		this.image_file_name = image_file_name;
 	}
 	
 	//イメージファイル画像高さ
-	public int getimage_file_height() {
+	public int getImage_file_height() {
 		return image_file_height;
 	}
 	
-	public void setimage_file_height(int image_file_height) {
+	public void setImage_file_height(int image_file_height) {
 		this.image_file_height = image_file_height;
 	}
 	
 	//イメージファイル画像幅
-	public int getimage_file_width() {
+	public int getImage_file_width() {
 		return image_file_width;
 	}
 	
-	public void setimage_file_width(int image_file_width) {
+	public void setImage_file_width(int image_file_width) {
 		this.image_file_width = image_file_width;
 	}
 	
 	//関連リンクURL
-	public String getother_link_url() {
+	public String getOther_link_url() {
 		return other_link_url;
 	}
 	
-	public void setother_link_url(String other_link_url) {
+	public void setOther_link_url(String other_link_url) {
 		this.other_link_url = other_link_url;
 	}
 	
 	//関連リンク文字列
-	public String getother_link_description() {
+	public String getOther_link_description() {
 		return other_link_description;
 	}
 	
-	public void setgetother_link_description(String other_link_description) {
+	public void setOther_link_description(String other_link_description) {
 		this.other_link_description = other_link_description;
 	}
 }
